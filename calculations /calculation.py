624f87a7923b9ac7621ddebc6948ddd7ef86ce2d
Jason <99057964@mydavinci.nl>
2021-11-03 14:39:32 +0100

    Lab opdracht aan het maken: De lunch feest af.
    Heb berekeningen in code getypt zodat het overzicht erover beter is. En een screenshot ervan gemaakt.

---
+++ calculations /calculation.py
@@ -0,0 +1,11 @@
+#Feest lunch.
+croissantjes = 17
+prijsCroissantjes = 0,39
+#Berekening voor de croissantjes = 17*0,39.
+stokbroden = 2
+prijsStokbroden = 2,78
+#Berekeninhg voor de stokbroden = 2*2,78.
+kortingsbon = 1,50
+#Het totale prijs korting.
+
+#Speel hal dag.
