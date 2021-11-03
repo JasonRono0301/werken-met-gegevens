b64f8f5509a784fd04a0a842e6f0c5fc2ea63182
Jason <99057964@mydavinci.nl>
2021-11-03 15:18:17 +0100

    Lab opdracht aan het maken: De speelhal dag af.
    Heb berekeningen in code getypt zodat het overzicht erover beter is. En een screenshot ervan gemaakt.

--- calculations /calculation.py
+++ calculations /calculation.py
@@ -1,11 +1,17 @@
 #Feest lunch.
 croissantjes = 17
-prijsCroissantjes = 0,39
+prijsCroissantjes = 0.39
 #Berekening voor de croissantjes = 17*0,39.
 stokbroden = 2
-prijsStokbroden = 2,78
+prijsStokbroden = 2.78
 #Berekeninhg voor de stokbroden = 2*2,78.
-kortingsbon = 1,50
+kortingsbon = 1.50
 #Het totale prijs korting.
 
 #Speel hal dag.
+personen = 3
+prijsPerpersoon = 7.45
+#Berekening voor de toegangsprijs = 3*7.45
+Viptijd = 9
+VipPrijs = 0.37
+#Berekening = 9*0.37
