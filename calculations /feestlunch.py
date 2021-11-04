e7b2d61ddf916eec8cc194a13a2d9c09d3a7c99f
Jason <99057964@mydavinci.nl>
2021-11-04 12:16:39 +0100

    De feest lunch opdracht af.

---
+++ calculations /feestlunch.py
@@ -0,0 +1,9 @@
+croissantjes = 17
+stokbroden = 2
+kortingsbonnen = 3
+
+prijsCroissant = 0.39
+prijsStokbrood = 2.78
+kortingsbonWaarde = 0.50
+
+print(croissantjes*prijsCroissant + stokbroden*prijsStokbrood - (kortingsbonnen*kortingsbonWaarde))
