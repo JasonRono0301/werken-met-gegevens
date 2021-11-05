9e14327955531afbc929f93d3448f21a202865b8
Jason <99057964@mydavinci.nl>
2021-11-05 14:01:38 +0100

    De feestlunch en de speelhaldag een nette output voor gemaakt.

--- calculations /feestlunch.py
+++ calculations /feestlunch.py
@@ -6,4 +6,16 @@ prijsCroissant = 0.39
 prijsStokbrood = 2.78
 kortingsbonWaarde = 0.50
 
-print(croissantjes*prijsCroissant + stokbroden*prijsStokbrood - (kortingsbonnen*kortingsbonWaarde))
+Croissantbedrag = croissantjes*prijsCroissant
+Stokbroodbedrag = stokbroden*prijsStokbrood
+
+Totalebedrag = croissantjes*prijsCroissant + stokbroden*prijsStokbrood - (kortingsbonnen*kortingsbonWaarde)
+
+feestlunchTekst = ('De feestlunch kost bij elkaar ' + str(Totalebedrag) + ' euro voor de ' + str(croissantjes) + ' croissantjes en de ' + str(stokbroden)
+    + ' stokbroden en de ' + str(kortingsbonnen) + ' kortingsbonnen als ze nog geldig zijn.')
+print(feestlunchTekst)
+
+
+
+
+
--- calculations /speelhaldag.py
+++ calculations /speelhaldag.py
@@ -3,4 +3,7 @@ aantalVipminuten = 9
 
 toegangsPrijs = 7.45
 vipPrijs = 0.37
-print(personen*toegangsPrijs + aantalVipminuten*vipPrijs)
+totaleSpeelhalbedrag = personen*toegangsPrijs + aantalVipminuten*vipPrijs
+
+speelhaldagTekst = ('Het totale speelhaldag bedrag is ' + str(totaleSpeelhalbedrag) + ' euro voor ' + str(personen) + ' personen en de 45 minuten vip tijd')
+print(speelhaldagTekst)
